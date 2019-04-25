1c301e635f47edb1f531929210495a371a062197
NicolasESchneider <nicolas.e.schneider@vanderbilt.edu>
2019-04-25 15:31:05 -0400

    webpack and base html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "static_pages#root"
   resources :calls, only: :create
   mount ActionCable.server, at: '/cable'
 end
--- frontend/VideoComponent.jsx
+++ frontend/VideoComponent.jsx
@@ -1,2 +1,7 @@
 import React from 'react';
 import VideoCall from './components/VideoCall';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<VideoCall />, root)
+})
